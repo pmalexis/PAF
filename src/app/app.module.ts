0edb87ffc59b8231e6dec2d02711ef5ce2b4bd13
Pierre_thiollent <pierre.thiollent76@gmail.com>
2018-12-20 12:08:51 +0100

    Affichage des différentes coiffures, possibilité d'aller voir une coiffure particulière

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoiffuresComponent } from './coiffures/coiffures.component';
+import { CoiffuresComponent} from "./coiffures/coiffures.component";
+import { CoiffureComponent } from './coiffure/coiffure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CoiffureComponent,
     CoiffuresComponent
   ],
   imports: [
